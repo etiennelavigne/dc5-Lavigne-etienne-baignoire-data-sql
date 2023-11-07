bbb568fd82da3a9fa9b9fab5d1af39e97c2dfc54
etiennelavigne <98097759+etiennelavigne@users.noreply.github.com>
2023-11-07 16:04:05 +0100

    Create ex8.sql
    V1 exo 8

---
+++ ex8.sql
@@ -0,0 +1,14 @@
+SELECT
+   o.name AS OfficerName,
+    i.name AS IntermediaryName,
+    aio.assoc_type,
+    aio.start_date,
+    aio.end_date
+FROM
+    assoc_inter_offi aio
+    JOIN officer o ON aio.officer = o.id
+    JOIN intermediary i ON aio.inter = i.id;
+
+
+
+--   Trouver tous les officiers et intermédiaires qui ont une association
